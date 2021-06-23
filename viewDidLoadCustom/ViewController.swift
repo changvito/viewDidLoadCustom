88b5cb0a8b9279a5ad99c2c2250a423720fec508
changvito <changvito@icloud.com>
2021-06-23 09:44:17 +0800

    viewDidLoad Trial

--- viewDidLoadCustom/ViewController.swift
+++ viewDidLoadCustom/ViewController.swift
@@ -12,6 +12,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Add Thanos gif into app
+        let thanosView = UIImageView(frame: CGRect(x: 40, y: 300, width: 300, height: 200))
+        
+        view.addSubview(thanosView)
+        
+        let thanosGif = UIImage.animatedImageNamed("thanosGif-", duration: 30)
+        
+        thanosView.image = thanosGif
+        
+        
+        //Add Fist Mask on Thanos gif
+        let maskImage = UIImage(named: "fistMask.png")
+        
+        let maskImageView = UIImageView(image: maskImage)
+        
+        thanosView.mask = maskImageView
+        
+        
+        //Add Night to the Background
+        let nightView = UIImageView(frame: CGRect(x: 0, y: 200, width: 400, height: 400))
+        
+        let nightImage = UIImage(named: "night.jpg")
+        
+        nightView.image = nightImage
+        
+        nightView.alpha = 0.5
+        
+        view.addSubview(nightView)
+        
+        
     }
 
 
